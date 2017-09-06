a8447e5d938bb88c06c4284a0b3f2d813c35b211
Momo Kornher <mrgrain@users.noreply.github.com>
2017-09-06 13:56:16 +1200

    Add installer files (#4)
    * Update dependencies
    
    * Add installer files from external project
    
    * Fix menu creation

---
+++ src/Installer/WordPress.php
@@ -0,0 +1,241 @@
+<?php
+namespace Autobahn\Installer;
+
+/**
+ * Class WordPress
+ *
+ * @package Autobahn\Installer
+ */
+class WordPress
+{
+    /**
+     * @var WordPressConfiguration Configuration object
+     */
+    protected $config;
+
+    /**
+     * Installer constructor.
+     *
+     * @param WordPressConfiguration $config The installer configuration
+     */
+    public function __construct(WordPressConfiguration $config)
+    {
+        $this->config = $config;
+    }
+
+    /**
+     * Registers the installer with the WordPress install hook
+     * and disables install defaults if required.
+     */
+    public function register()
+    {
+        // Maybe disable WordPress defaults
+        if (!$this->config->defaults()) {
+            $this->disableDefaults();
+        }
+
+        add_action('wp_install', [$this, 'install'], PHP_INT_MAX);
+    }
+
+    /**
+     * Run the install scripts.
+     */
+    public function install()
+    {
+        // 1. Set options
+        $this->setOptions($this->config->options());
+
+        // 2. Activate plugins
+        $this->activatePlugins($this->config->plugins());
+
+        // 3. Maybe set the theme (and settings)
+        if ($theme = $this->config->theme()) {
+            $this->setTheme($theme->name, $theme->mods, $theme->options, $theme->menus);
+        }
+
+        // 4. Maybe set the permalink structure
+        global $wp_rewrite;
+        if ($permalinks = $this->config->permalinks() && $wp_rewrite) {
+            $this->setPermalinkStructure($wp_rewrite, $permalinks);
+        }
+
+        // 5. Maybe set the timezone
+        if ($timezone = $this->config->timezone()) {
+            $this->setTimezone($timezone);
+        }
+    }
+
+    /**
+     * Disables the WordPress install defaults.
+     */
+    public function disableDefaults()
+    {
+        // Overwrite wp_install_defaults functions here if it doesn't exist yet.
+        // functions declared inside a function have global scope
+        if (!function_exists('wp_install_defaults')) :
+            function wp_install_defaults($user_id)
+            {
+            }
+        endif;
+    }
+
+    /**
+     * Set or expand a list if WordPress options.
+     *
+     * @param array $options The list of options to be set
+     */
+    public function setOptions($options)
+    {
+        foreach ($options as $option => $value) {
+            $this->setOption($option, $value);
+        }
+    }
+
+    /**
+     * Activated all given plugins.
+     *
+     * @param array $plugins List of plugins to be activated.
+     */
+    public function activatePlugins($plugins)
+    {
+        foreach ($plugins as $plugin) {
+            activate_plugin($plugin);
+        }
+    }
+
+    /**
+     * Set a theme and possible mods, related options and menus.
+     *
+     * @param string $theme
+     * @param array  $mods
+     * @param array  $options
+     * @param array  $menus
+     */
+    public function setTheme($theme, array $mods = [], array $options = [], array $menus = [])
+    {
+        $this->setOptions($options);
+        switch_theme($theme);
+        $this->setThemeMods($mods);
+        $this->createMenus($menus);
+    }
+
+    /**
+     * Set and flush the permalink structure.
+     *
+     * @param WP_Rewrite $wp_rewrite The WordPress rewrite engine.
+     * @param string     $structure  The permalink structure to be used.
+     */
+    public function setPermalinkStructure($wp_rewrite, $structure)
+    {
+        // Set the new permalinks structure
+        $wp_rewrite->set_permalink_structure($structure);
+        // Flush the rules and tell it to write htaccess
+        $wp_rewrite->flush_rules(true);
+
+        // Clear the rewrite rules options cache
+        update_option("rewrite_rules", false);
+    }
+
+    /**
+     * Set the WordPress timezone string setting and ensures gmt offset is not used.
+     *
+     * @param string $timezone The timezone string to be set.
+     */
+    public function setTimezone($timezone)
+    {
+        delete_option('gmt_offset');
+        update_option('timezone_string', $timezone);
+    }
+
+    /**
+     * Set or expand a single WordPress option.
+     * If the given $value is an array, extend the data by reading it first if $extend flag is set.
+     *
+     * @param string $option The name of the option to be set.
+     * @param mixed  $value  The new value for the option.
+     * @param bool   $extend Enables extending for sub options.
+     */
+    protected function setOption($option, $value, $extend = true)
+    {
+        // for arrays as value, extend the value
+        if ($extend && is_array($value)) {
+            $current = get_option($option, []);
+            foreach ($value as $sub_option => $sub_value) {
+                $current[$sub_option] = $sub_value;
+            }
+            $value = $current;
+        }
+
+        // store the option
+        update_option($option, $value);
+    }
+
+    /**
+     * Set theme mods.
+     *
+     * @param array $themeMods
+     */
+    protected function setThemeMods($themeMods)
+    {
+        foreach ($themeMods as $option => $value) {
+            set_theme_mod($option, $value);
+        }
+    }
+
+    /**
+     * Create, fill and place WordPress menus.
+     *
+     * @param array $menus
+     */
+    protected function createMenus($menus)
+    {
+        $locations = [];
+
+        foreach ($menus as $menu) {
+            $menu_id = $this->createMenu($menu['name']);
+
+            if ($menu_id) {
+                $locations[$menu['location']] = $menu_id;
+                $this->populateMenu($menu_id, $menu['items']);
+            }
+        }
+
+        set_theme_mod('nav_menu_locations', $locations);
+    }
+
+    /**
+     * Create a new WordPress menu.
+     *
+     * @param string $menu_name
+     *
+     * @return bool|int|WP_Error
+     */
+    protected function createMenu($menu_name)
+    {
+        $menu_id = wp_create_nav_menu($menu_name);
+
+        if (!is_object($menu_id) && intval($menu_id) > 0) {
+            return $menu_id;
+        }
+
+        return false;
+    }
+
+    /**
+     * Add menu items to menu.
+     *
+     * @param integer $menu_id    The menu ID
+     * @param array   $menu_items List of menu items
+     */
+    protected function populateMenu($menu_id, $menu_items)
+    {
+        foreach ($menu_items as $menu_item) {
+            wp_update_nav_menu_item($menu_id, 0, [
+                'menu-item-status' => 'publish',
+                'menu-item-type'   => isset($menu_item['type']) ? $menu_item['type'] : 'custom',
+                'menu-item-title'  => $menu_item['title'],
+                'menu-item-url'    => $menu_item['url']
+            ]);
+        }
+    }
+}
---
+++ src/Installer/WordPressConfiguration.php
@@ -0,0 +1,228 @@
+<?php
+namespace Autobahn\Installer;
+
+/**
+ * Class WordPressConfiguration
+ *
+ * @package Autobahn\Installer
+ */
+class WordPressConfiguration
+{
+    /**
+     * @var array
+     */
+    protected $options = [];
+
+    /**
+     * @var array
+     */
+    protected $plugins = true;
+
+    /**
+     * @var boolean|object
+     */
+    protected $theme = false;
+
+    /**
+     * @var boolean|string
+     */
+    protected $permalinks = true;
+
+    /**
+     * @var boolean|string
+     */
+    protected $timezone = true;
+
+    /**
+     * @var boolean
+     */
+    protected $defaults = false;
+
+    /**
+     * InstallerConfiguration constructor.
+     *
+     * @param string $content A configuration string.
+     */
+    public function __construct($content)
+    {
+        // check if config is valid
+        if (!static::isValidConfiguration($content)) {
+            throw new \InvalidArgumentException('Install configuration file is invalid.');
+        }
+
+        // convert to object
+        $config = json_decode($content, true);
+
+        // set properties
+        if (isset($config['options'])) {
+            $this->options = $config['options'];
+        }
+        if (isset($config['plugins'])) {
+            $this->plugins = $config['plugins'];
+        }
+        if (isset($config['theme'])) {
+            $this->theme = (object)$config['theme'];
+        }
+        if (isset($config['permalinks'])) {
+            $this->permalinks = $config['permalinks'];
+        }
+        if (isset($config['timezone'])) {
+            $this->timezone = $config['timezone'];
+        }
+        if (isset($config['defaults'])) {
+            $this->defaults = $config['defaults'];
+        }
+    }
+
+    /**
+     * Create a configuration from a file path.
+     *
+     * @param string $file Path to the configuration file.
+     *
+     * @return self
+     */
+    public static function fromFile($file)
+    {
+        $content = @file_get_contents($file);
+
+        return new static($content);
+    }
+
+    /**
+     * Check if the install configuration is valid.
+     *
+     * @todo Actually check parts of config for sanity.
+     *
+     * @param string $content
+     *
+     * @return bool
+     */
+    public static function isValidConfiguration($content)
+    {
+        json_decode($content);
+
+        return (json_last_error() == JSON_ERROR_NONE);
+    }
+
+
+    /**
+     * Get the options configuration.
+     *
+     * @return array
+     */
+    public function options()
+    {
+        return $this->options ?: [];
+    }
+
+    /**
+     * Get a list of plugins to be activated.
+     *
+     * @return array
+     */
+    public function plugins()
+    {
+        // plugins is a list of arrays, activate on a per plugin base
+        if (is_array($this->plugins)) {
+            return $this->plugins;
+        }
+
+        // for anything the evaluates to true, activate all plugins
+        if ($this->plugins) {
+            $plugins = apply_filters('all_plugins', get_plugins());
+
+            return array_keys($plugins);
+        }
+
+        return [];
+    }
+
+    /**
+     * Get the theme object or false if not to be set.
+     *
+     * @return bool|object
+     */
+    public function theme()
+    {
+        // nothing set, skip theme
+        if (!$this->theme || !isset($this->theme->name)) {
+            return false;
+        }
+
+        // only theme name is set
+        if (is_string($this->theme)) {
+            return (object)[
+                'name'    => $this->theme,
+                'mods'    => [],
+                'options' => [],
+                'menus'   => []
+            ];
+        }
+
+        // read theme setting
+        return (object)[
+            'name'    => $this->theme->name,
+            'mods'    => isset($this->theme->mods) ? $this->theme->mods : [],
+            'options' => isset($this->theme->options) ? $this->theme->options : [],
+            'menus'   => isset($this->theme->menus) ? $this->theme->menus : []
+        ];
+    }
+
+    /**
+     * Get the permalinks structure or false if not to be set.
+     *
+     * @return string
+     */
+    public function permalinks()
+    {
+        if (true === $this->permalinks) {
+            return $this->getDefaultPermalinks();
+        }
+
+        return $this->permalinks;
+    }
+
+    /**
+     * Get the timezone string or false if not to be set.
+     *
+     * @return string|boolean
+     */
+    public function timezone()
+    {
+        if (true === $this->timezone) {
+            return $this->getDefaultTimezone();
+        }
+
+        return $this->timezone;
+    }
+
+    /**
+     * Get the setting if WordPress install defaults are to be used.
+     *
+     * @return boolean
+     */
+    public function defaults()
+    {
+        return $this->defaults;
+    }
+
+    /**
+     * Get the default timezone value.
+     *
+     * @return string
+     */
+    protected function getDefaultTimezone()
+    {
+        return ini_get('date.timezone');
+    }
+
+    /**
+     * Get the default permalinks value.
+     *
+     * @return string
+     */
+    protected function getDefaultPermalinks()
+    {
+        return "/%postname%/";
+    }
+}
